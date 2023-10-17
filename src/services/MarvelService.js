002642a2ba70a8f141e02ea7d4785520b8c29e87
Sapunov <ilsapunov@gmail.com>
2023-10-17 21:13:37 +0300

    add load more chars

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = process.env.REACT_APP_MARVEL_API_KEY;
+    _baseOffset = 210;
     
 
     getResource = async (url) => {
@@ -13,8 +14,8 @@ class MarvelService {
         return await res.json();
     }
     
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&apikey=${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&apikey=${this._apiKey}`);
         return res.data.results.map(this._transformCharacter)
     }
     getCharacter = async (id) => {
